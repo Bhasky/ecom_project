ee53f41bff106d13e1adeda5eaca5bfdbc6e3d3e
Bhasky <bhasky.aug11@gmail.com>
2024-04-29 21:37:51 +0530

    login issue fixed

--- shopping/src/app/admin/admin-login/admin-login.component.ts
+++ shopping/src/app/admin/admin-login/admin-login.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 import { user } from 'src/admin.model';
 import { AdminuserService } from '../adminuser.service';
 import { Router } from '@angular/router';
-import {NgForm} from '@angular/forms';
- 
+import { NgForm } from '@angular/forms';
+import { AuthserviceService } from '../Service/authservice.service';
+
 
 
 
@@ -15,34 +16,46 @@ import {NgForm} from '@angular/forms';
   styleUrls: ['./admin-login.component.css']
 })
 export class AdminLoginComponent {
-  allUsers: user[]=[];
-  errormessage:string='';
+  allUsers: user[] = [];
+  errormessage: string = '';
 
-  constructor(private adminuser:AdminuserService, private router:Router){}
+  userForm={
+    login:'',
+    password:''
+  }
+
+  constructor(private adminuser: AdminuserService, private router: Router) { }
 
 
-  login(myform:any){
-    let loginUser: user={
-      uname:myform.value.uname,
-      upassword:myform.value.upassword,
-      token:''
+  login(myform: any) {
+    let loginUser: user = {
+      uname: myform.value.uname,
+      upassword: myform.value.upassword,
+      token: ''
     }
-this.adminuser.fetchAllUsers().subscribe({
-next:(response)=>
-{this.allUsers=response;
-let filterUser:user[] = this.allUsers.filter((eachUser)=>{
-  return(eachUser.uname==loginUser.uname && eachUser.upassword==loginUser.upassword)
-});
-if(filterUser.length==1){
-  this.router.navigate(['admin/orders']);
-}
-else{
-  this.errormessage="Invalid Username & password";
-}
-},
-error:(err)=>{console.log(err)}
-})
+    this.adminuser.fetchAllUsers().subscribe({
+      next: (response) => {
+        this.allUsers = response;
+       
 
-    
+        let filterUser: user[] = this.allUsers.filter((eachUser) => {
+          
+
+          return (eachUser.uname == loginUser.uname && eachUser.upassword == loginUser.upassword)
+        });
+        
+        if (filterUser.length == 1) {
+      
+          this.router.navigate(['admin/orders']);
+        }
+        else {
+          this.errormessage = "Invalid Username & password";
+          
+        }
+      },
+      error: (err) => { console.log(err) }
+    })
+
+
   }
 }
